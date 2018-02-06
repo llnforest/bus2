df1adad368ec1459c73c7e171a32847215d36bfb
llnforest <1220343451@qq.com>
2018-02-06 09:09:32 +0800

    'bug'

--- appadmin/reserve/view/order/index.php
+++ appadmin/reserve/view/order/index.php
@@ -72,7 +72,7 @@
                     <td><span class="span-primary order-detail" data-id="{$v.id}">{$v.id}</span></td>
                     <td>{$v.name}</td>
                     <td>{if $v.status == 1}已派单{elseif $v.status == 2}<span class="blue">交易成功</span>{elseif $v.status == 3}<span class="grey">交易取消</span>{else}<span class="red">待派单</span>{/if}</td>
-                    <td>{if $v.order_type == 1}旅行社用车{elseif $v.order_type == 2}交通车{elseif order_type == 3}团车{elseif order_type == 4}社会用车{/if}</td>
+                    <td>{if $v.order_type == 1}旅行社用车{elseif $v.order_type == 2}交通车{elseif $v.order_type == 3}团车{elseif $v.order_type == 4}社会用车{/if}</td>
                     <td>{if $v.type == 1}全包{elseif $v.type == 2}净价{/if}</td>
                     <td>
                         {if $v.order_type == 1}
